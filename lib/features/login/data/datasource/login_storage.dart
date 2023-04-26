e20b3afab7c8d03f826650abf93bab09b04c77fc
Zewodec <zewodec@gmail.com>
2023-04-27 01:24:27 +0300

    Login Page Done Storage save/Load done Add Fonts

---
+++ lib/features/login/data/datasource/login_storage.dart
@@ -0,0 +1,58 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LoginStorage {
+  const LoginStorage._();
+
+  static const String _sessionTokenKey = "_sessionToken";
+  static const String _signatureKey = "_signature";
+  static const String _deviceIdKey = "_deviceId";
+  static const String _accessTokenKey = "_accessToken";
+
+  static Future<void> setSessionToken(String sessionToken) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    prefs.setString(_sessionTokenKey, sessionToken);
+  }
+
+  static Future<String?> getSessionToken() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getString(_sessionTokenKey);
+  }
+
+  static Future<void> setSignature(String signature) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    prefs.setString(_signatureKey, signature);
+  }
+
+  static Future<String?> getSignature() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getString(_signatureKey);
+  }
+
+  static Future<void> setDeviceId(String deviceId) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    prefs.setString(_deviceIdKey, deviceId);
+  }
+
+  static Future<String?> getDeviceId() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getString(_deviceIdKey);
+  }
+
+  static Future<void> setAccessToken(String accessToken) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    prefs.setString(_accessTokenKey, accessToken);
+  }
+
+  static Future<String?> getAccessToken() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getString(_accessTokenKey);
+  }
+}
--- lib/features/login/presentation/pages/login_page.dart
+++ lib/features/login/presentation/pages/login_page.dart
@@ -1,17 +1,127 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+  late TextEditingController _phoneNumberFieldController;
+
+  // String _phoneNumber = "";
+
+  @override
+  void initState() {
+    _phoneNumberFieldController = TextEditingController();
+    _phoneNumberFieldController.text = "+38";
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _phoneNumberFieldController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Авторизація",
+          style: TextStyle(fontFamily: "FixelDisplay"),
+        ),
+      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-          ],
+        child: Form(
+          key: _loginFormKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                "Авторизація",
+                style: TextStyle(
+                  fontFamily: "FixelDisplay",
+                  fontSize: 30,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                width: 300,
+                child: TextFormField(
+                  controller: _phoneNumberFieldController,
+                  keyboardType: TextInputType.phone,
+                  decoration: const InputDecoration(
+                      labelText: "Номер телефону",
+                      labelStyle: TextStyle(
+                        fontFamily: "FixelDisplay",
+                      ),
+                      hintText: "Ваш номер телефону",
+                      hintStyle:
+                          TextStyle(fontFamily: "FixelText", fontSize: 12),
+                      errorStyle:
+                          TextStyle(fontFamily: "FixelText", fontSize: 12)),
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return "Введіть номер телефону, будь ласка";
+                    }
+                    // Check if the phone number has the correct length and starts with the correct prefix
+                    if (RegExp(r'^\+380\d{9}$').hasMatch(value)) {
+                      return null; // Return null if the input is valid
+                    } else {
+                      return 'Введіть коректно номер телефону'; // Return an error message if the input is invalid
+                    }
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                    onPressed: () =>
+                        {if (_loginFormKey.currentState!.validate()) {}},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          "Авторизуватись за ",
+                          style: TextStyle(fontFamily: "FixelText"),
+                        ),
+                        Icon(Icons.phone)
+                      ],
+                    )),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              SizedBox(
+                width: 200,
+                height: 30,
+                child: OutlinedButton(
+                    onPressed: () => {},
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Text(
+                          "Авторизуватись як гість ",
+                          style: TextStyle(
+                              fontFamily: "FixelText",
+                              fontSize: 11,
+                              fontStyle: FontStyle.italic),
+                        ),
+                        Icon(Icons.people)
+                      ],
+                    )),
+              ),
+            ],
+          ),
         ),
       ),
     );
